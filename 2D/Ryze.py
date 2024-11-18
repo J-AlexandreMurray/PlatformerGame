fb3294400f5a6576c75d323011f5fbbb019a5f79
J-AlexandreMurray <john_murray13@outlook.com>
2024-11-18 14:40:53 +0100

    more code refactoring

--- 2D/Ryze.py
+++ 2D/Ryze.py
@@ -12,8 +12,8 @@ pygame.init()
 clock = pygame.time.Clock()
 fps = 60
 
-screen_width = 1000
-screen_height = 1000
+screen_width = 800
+screen_height = 800
 
 screen = pygame.display.set_mode((screen_width, screen_height))
 pygame.display.set_caption('A new change is coming - sidescroller...')
